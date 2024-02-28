53e034d8d14592f7773e224e30e0cfa6d240b5b2
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-02-28 11:37:19 +0100

    install snapshop and file-system packages

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,8 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Region } from "react-native-maps";
 import React, { useRef, useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
+import * as FileSystem from "expo-file-system";
+import { shareAsync } from "expo-sharing";
 
 const INITIAL_REGION = {
   latitude: 6.335001,
@@ -26,6 +28,21 @@ const index = () => {
   const zoom = () => {
     mapRef.current?.animateCamera({ zoom: 13 });
   };
+
+  const takeSnapshotAndShare = async () => {
+    const snapshot = await mapRef.current?.takeSnapshot({
+      width: 300,
+      height: 300,
+      result: "base64",
+    });
+
+    const uri = FileSystem.documentDirectory + "snapshot.png";
+    await FileSystem.writeAsStringAsync(uri, snapshot!, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    await shareAsync(uri);
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <MapView
@@ -46,6 +63,9 @@ const index = () => {
         </TouchableOpacity>
         <TouchableOpacity style={styles.btn} onPress={zoom}>
           <Ionicons name="earth" size={24} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={takeSnapshotAndShare}>
+          <Ionicons name="camera" size={24} />
         </TouchableOpacity>
       </View>
     </View>
